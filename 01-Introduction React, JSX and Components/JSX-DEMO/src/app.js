b106d4a6f9b31cefdf597e83c25d4bd8bbc127cf
Martin Marinov <martin.marinov10@yahoo.com>
2025-11-02 16:40:56 +0000

    Change the React import method

--- 01-Introduction React, JSX and Components/JSX-DEMO/src/app.js
+++ 01-Introduction React, JSX and Components/JSX-DEMO/src/app.js
@@ -1,3 +1,5 @@
+import React from "https://esm.sh/react@19.2.0";
+
 const App = React.createElement('h1', null, 'My First React APP!');
 
 export default App;
--- 01-Introduction React, JSX and Components/JSX-DEMO/src/index.js
+++ 01-Introduction React, JSX and Components/JSX-DEMO/src/index.js
@@ -1,3 +1,5 @@
+import ReactDOM from "https://esm.sh/react-dom@19.2.0/client";
+
 import App from "./app.js";
 
 const rootDomElement = document.getElementById('root');
