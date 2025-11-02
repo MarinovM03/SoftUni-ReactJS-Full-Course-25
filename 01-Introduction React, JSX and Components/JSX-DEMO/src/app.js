b083765c9893add7ad9c351e2fcf0279464eca8e
Martin Marinov <martin.marinov10@yahoo.com>
2025-11-02 16:31:10 +0000

    Init React & JSX demo

---
+++ 01-Introduction React, JSX and Components/JSX-DEMO/src/app.js
@@ -0,0 +1,3 @@
+const App = React.createElement('h1', null, 'My First React APP!');
+
+export default App;
---
+++ 01-Introduction React, JSX and Components/JSX-DEMO/src/index.js
@@ -0,0 +1,6 @@
+import App from "./app.js";
+
+const rootDomElement = document.getElementById('root');
+const rootReactElement = ReactDOM.createRoot(rootDomElement);
+
+rootReactElement.render(App);
