64d1e960c33d1382ca37bb427a20a8d38bd10a78
Martin Marinov <martin.marinov10@yahoo.com>
2025-11-02 16:45:44 +0000

    Add subheading to React app and wrap in div using basic JS

--- 01-Introduction React, JSX and Components/JSX-DEMO/src/app.js
+++ 01-Introduction React, JSX and Components/JSX-DEMO/src/app.js
@@ -1,5 +1,8 @@
 import React from "https://esm.sh/react@19.2.0";
 
-const App = React.createElement('h1', null, 'My First React APP!');
+const h1ReactElement = React.createElement('h1', null, 'My First React APP!');
+const h2ReactElement = React.createElement('h2', null, 'The best framework ever!');
+
+const App = React.createElement('div', {className: 'app-container'}, h1ReactElement, h2ReactElement);
 
 export default App;
