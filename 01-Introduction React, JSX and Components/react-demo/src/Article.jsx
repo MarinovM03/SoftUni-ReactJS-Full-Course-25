cc1d0b726e5ce5e5e57783c1cf12a4e8e6a00503
Martin Marinov <martin.marinov10@yahoo.com>
2025-11-07 20:26:32 +0000

    Add Article component and use it inside the Body component

---
+++ 01-Introduction React, JSX and Components/react-demo/src/Article.jsx
@@ -0,0 +1,7 @@
+export default function Article() {
+    return (
+        <article>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, molestiae!</p>
+        </article>
+    );
+};
--- 01-Introduction React, JSX and Components/react-demo/src/Body.jsx
+++ 01-Introduction React, JSX and Components/react-demo/src/Body.jsx
@@ -1,8 +1,10 @@
+import Article from "./Article.jsx";
+
 export default function Body() {
     return (
         <section>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, molestiae!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, maiores.</p>
+            <Article />
+            <Article />
         </section>
     );
 };
