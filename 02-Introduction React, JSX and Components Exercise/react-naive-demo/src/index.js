5e0209bc05dbf04175d92ab38e3a6998baa84ee8
Martin Marinov <martin.marinov10@yahoo.com>
2025-11-14 20:58:27 +0000

    Add nested elements example

--- 02-Introduction React, JSX and Components Exercise/react-naive-demo/src/index.js
+++ 02-Introduction React, JSX and Components Exercise/react-naive-demo/src/index.js
@@ -1,5 +1,10 @@
 import React from "./react.js";
 
-const reactElement = React.createElement('h1', { className: 'site-header' }, 'My first React naive app');
+const reactElement = React.createElement(
+    'div',
+    null,
+    React.createElement('h1', null, 'My first React naive app'),
+    React.createElement('h2', null, 'Awesome Simple'),
+);
 
 console.log(reactElement);
