c07042a2a401f4f222eea20346fbf998478f5e8b
Martin Marinov <martin.marinov10@yahoo.com>
2025-11-14 18:16:10 +0000

    Add Blog and ContactUs components

--- 02-Introduction React, JSX and Components Exercise/html-template-demo/src/App.jsx
+++ 02-Introduction React, JSX and Components Exercise/html-template-demo/src/App.jsx
@@ -1,5 +1,7 @@
 import BigFeatureRight from "./components/BigFeatureRight.jsx"
 import BigFeaturesLeft from "./components/BigFeaturesLeft.jsx"
+import Blog from "./components/Blog.jsx"
+import ContactUs from "./components/ContactUs.jsx"
 import Counter from "./components/Counter.jsx"
 import Features from "./components/Features.jsx"
 import Footer from "./components/Footer.jsx"
@@ -12,7 +14,7 @@ import WelcomeArea from "./components/WelcomeArea.jsx"
 
 function App() {
     return (
-        <main>
+        <>
             <Preloader />
 
             <Header />
@@ -33,146 +35,12 @@ function App() {
 
             <Counter />
 
-            {/* <!-- ***** Blog Start ***** --> */}
-            <section className="section" id="blog">
-                <div className="container">
-                    {/* <!-- ***** Section Title Start ***** --> */}
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="center-heading">
-                                <h2 className="section-title">Blog Entries</h2>
-                            </div>
-                        </div>
-                        <div className="offset-lg-3 col-lg-6">
-                            <div className="center-text">
-                                <p>Integer molestie aliquam gravida. Nullam nec arcu finibus, imperdiet nulla vitae, placerat nibh. Cras maximus venenatis molestie.</p>
-                            </div>
-                        </div>
-                    </div>
-                    {/* <!-- ***** Section Title End ***** --> */}
+            <Blog />
 
-                    <div className="row">
-                        <div className="col-lg-4 col-md-6 col-sm-12">
-                            <div className="blog-post-thumb">
-                                <div className="img">
-                                    <img src="assets/images/blog-item-01.png" alt="" />
-                                </div>
-                                <div className="blog-content">
-                                    <h3>
-                                        <a href="#">Vivamus ac vehicula dui</a>
-                                    </h3>
-                                    <div className="text">
-                                    Cras aliquet ligula dui, vitae fermentum velit tincidunt id. Praesent eu finibus nunc. Nulla in sagittis eros. Aliquam egestas augue.
-                                    </div>
-                                    <a href="#" className="main-button">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 col-md-6 col-sm-12">
-                            <div className="blog-post-thumb">
-                                <div className="img">
-                                    <img src="assets/images/blog-item-02.png" alt="" />
-                                </div>
-                                <div className="blog-content">
-                                    <h3>
-                                        <a href="#">Phasellus convallis augue</a>
-                                    </h3>
-                                    <div className="text">
-                                        Aliquam commodo ornare nisl, et scelerisque nisl dignissim ac. Vestibulum finibus urna ut velit venenatis, vel ultrices sapien mattis.
-                                    </div>
-                                    <a href="#" className="main-button">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-4 col-md-6 col-sm-12">
-                            <div className="blog-post-thumb">
-                                <div className="img">
-                                    <img src="assets/images/blog-item-03.png" alt="" />
-                                </div>
-                                <div className="blog-content">
-                                    <h3>
-                                        <a href="#">Nam gravida purus non</a>
-                                    </h3>
-                                    <div className="text">
-                                        Maecenas eu erat vitae dui convallis consequat vel gravida nulla. Vestibulum finibus euismod odio, ut tempus enim varius eu.
-                                    </div>
-                                    <a href="#" className="main-button">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            {/* <!-- ***** Blog End ***** --> */}
+            <ContactUs />
 
-            {/* <!-- ***** Contact Us Start ***** --> */}
-            <section className="section colored" id="contact-us">
-                <div className="container">
-                    {/* <!-- ***** Section Title Start ***** --> */}
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="center-heading">
-                                <h2 className="section-title">Talk To Us</h2>
-                            </div>
-                        </div>
-                        <div className="offset-lg-3 col-lg-6">
-                            <div className="center-text">
-                                <p>Maecenas pellentesque ante faucibus lectus vulputate sollicitudin. Cras feugiat hendrerit semper.</p>
-                            </div>
-                        </div>
-                    </div>
-                    {/* <!-- ***** Section Title End ***** --> */}
-
-                    <div className="row">
-                        {/* <!-- ***** Contact Text Start ***** --> */}
-                        <div className="col-lg-4 col-md-6 col-sm-12">
-                            <h5 className="margin-bottom-30">Keep in touch</h5>
-                            <div className="contact-text">
-                                <p>110-220 Quisque diam odio, maximus ac consectetur eu, 10260
-                                <br />auctor non lorem</p>
-                                <p>You are NOT allowed to re-distribute Softy Pinko template on any template collection websites. Thank you.</p>
-                            </div>
-                        </div>
-                        {/* <!-- ***** Contact Text End ***** --> */}
-
-                        {/* <!-- ***** Contact Form Start ***** --> */}
-                        <div className="col-lg-8 col-md-6 col-sm-12">
-                            <div className="contact-form">
-                                <form id="contact" action="" method="get">
-                                <div className="row">
-                                    <div className="col-lg-6 col-md-12 col-sm-12">
-                                    <fieldset>
-                                        <input name="name" type="text" className="form-control" id="name" placeholder="Full Name" required="" />
-                                    </fieldset>
-                                    </div>
-                                    <div className="col-lg-6 col-md-12 col-sm-12">
-                                    <fieldset>
-                                        <input name="email" type="email" className="form-control" id="email" placeholder="E-Mail Address" required="" />
-                                    </fieldset>
-                                    </div>
-                                    <div className="col-lg-12">
-                                    <fieldset>
-                                        <textarea name="message" rows="6" className="form-control" id="message" placeholder="Your Message" required=""></textarea>
-                                    </fieldset>
-                                    </div>
-                                    <div className="col-lg-12">
-                                    <fieldset>
-                                        <button type="submit" id="form-submit" className="main-button">Send Message</button>
-                                    </fieldset>
-                                    </div>
-                                </div>
-                                </form>
-                            </div>
-                        </div>
-                        {/* <!-- ***** Contact Form End ***** --> */}
-                    </div>
-                </div>
-            </section>
-            {/* <!-- ***** Contact Us End ***** --> */}
-
-            {/* <!-- ***** Footer Start ***** --> */}
             <Footer />
-        </main>
+        </>
     )
 }
 
