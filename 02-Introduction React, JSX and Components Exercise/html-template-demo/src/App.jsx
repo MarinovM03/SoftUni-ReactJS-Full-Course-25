55e31e5719a4f845de6b606a7fc620bdcec8314a
Martin Marinov <martin.marinov10@yahoo.com>
2025-11-14 18:10:51 +0000

    Add HomeParallax and Testimonials components

--- 02-Introduction React, JSX and Components Exercise/html-template-demo/src/App.jsx
+++ 02-Introduction React, JSX and Components Exercise/html-template-demo/src/App.jsx
@@ -3,7 +3,9 @@ import BigFeaturesLeft from "./components/BigFeaturesLeft.jsx"
 import Features from "./components/Features.jsx"
 import Footer from "./components/Footer.jsx"
 import Header from "./components/Header.jsx"
+import HomeParallax from "./components/HomeParallax.jsx"
 import Preloader from "./components/Preloader.jsx"
+import Testimonials from "./components/Testimonials.jsx"
 import WelcomeArea from "./components/WelcomeArea.jsx"
 
 function App() {
@@ -21,146 +23,9 @@ function App() {
 
             <BigFeatureRight />
 
-            {/* ***** Home Parallax Start ***** */}
-            <section className="mini" id="work-process">
-                <div className="mini-content">
-                    <div className="container">
-                        <div className="row">
-                            <div className="offset-lg-3 col-lg-6">
-                                <div className="info">
-                                    <h1>Work Process</h1>
-                                    <p>Aenean nec tempor metus. Maecenas ligula dolor, commodo in imperdiet interdum, vehicula ut ex. Donec ante diam.</p>
-                                </div>
-                            </div>
-                        </div>
+            <HomeParallax />
 
-                        {/* <!-- ***** Mini Box Start ***** --> */}
-                        <div className="row">
-                            <div className="col-lg-2 col-md-3 col-sm-6 col-6">
-                                <a href="#" className="mini-box">
-                                    <i><img src="assets/images/work-process-item-01.png" alt="" /></i>
-                                    <strong>Get Ideas</strong>
-                                    <span>Godard pabst prism fam cliche.</span>
-                                </a>
-                            </div>
-                            <div className="col-lg-2 col-md-3 col-sm-6 col-6">
-                                <a href="#" className="mini-box">
-                                    <i><img src="assets/images/work-process-item-01.png" alt="" /></i>
-                                    <strong>Sketch Up</strong>
-                                    <span>Godard pabst prism fam cliche.</span>
-                                </a>
-                            </div>
-                            <div className="col-lg-2 col-md-3 col-sm-6 col-6">
-                                <a href="#" className="mini-box">
-                                    <i><img src="assets/images/work-process-item-01.png" alt="" /></i>
-                                    <strong>Discuss</strong>
-                                    <span>Godard pabst prism fam cliche.</span>
-                                </a>
-                            </div>
-                            <div className="col-lg-2 col-md-3 col-sm-6 col-6">
-                                <a href="#" className="mini-box">
-                                    <i><img src="assets/images/work-process-item-01.png" alt="" /></i>
-                                    <strong>Revise</strong>
-                                    <span>Godard pabst prism fam cliche.</span>
-                                </a>
-                            </div>
-                            <div className="col-lg-2 col-md-3 col-sm-6 col-6">
-                                <a href="#" className="mini-box">
-                                    <i><img src="assets/images/work-process-item-01.png" alt="" /></i>
-                                    <strong>Approve</strong>
-                                    <span>Godard pabst prism fam cliche.</span>
-                                </a>
-                            </div>
-                            <div className="col-lg-2 col-md-3 col-sm-6 col-6">
-                                <a href="#" className="mini-box">
-                                    <i><img src="assets/images/work-process-item-01.png" alt="" /></i>
-                                    <strong>Launch</strong>
-                                    <span>Godard pabst prism fam cliche.</span>
-                                </a>
-                            </div>
-                        </div>
-                        {/* <!-- ***** Mini Box End ***** --> */}
-                    </div>
-                </div>
-            </section>
-            {/* <!-- ***** Home Parallax End ***** --> */}
-
-            {/* <!-- ***** Testimonials Start ***** --> */}
-            <section className="section" id="testimonials">
-                <div className="container">
-                    {/* <!-- ***** Section Title Start ***** --> */}
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="center-heading">
-                                <h2 className="section-title">What do they say?</h2>
-                            </div>
-                        </div>
-                        <div className="offset-lg-3 col-lg-6">
-                            <div className="center-text">
-                                <p>Donec tempus, sem non rutrum imperdiet, lectus orci fringilla nulla, at accumsan elit eros a turpis. Ut sagittis lectus libero.</p>
-                            </div>
-                        </div>
-                    </div>
-                    {/* <!-- ***** Section Title End ***** --> */}
-
-                    <div className="row">
-                        {/* <!-- ***** Testimonials Item Start ***** --> */}
-                        <div className="col-lg-4 col-md-6 col-sm-12">
-                            <div className="team-item">
-                                <div className="team-content">
-                                    <i><img src="assets/images/testimonial-icon.png" alt="" /></i>
-                                    <p>Proin a neque nisi. Nam ipsum nisi, venenatis ut nulla quis, egestas scelerisque orci. Maecenas a finibus odio.</p>
-                                    <div className="user-image">
-                                        <img src="http://placehold.it/60x60" alt="" />
-                                    </div>
-                                    <div className="team-info">
-                                        <h3 className="user-name">Catherine Soft</h3>
-                                        <span>Managing Director</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* <!-- ***** Testimonials Item End ***** --> */}
-                        
-                        {/* <!-- ***** Testimonials Item Start ***** --> */}
-                        <div className="col-lg-4 col-md-6 col-sm-12">
-                            <div className="team-item">
-                                <div className="team-content">
-                                    <i><img src="assets/images/testimonial-icon.png" alt="" /></i>
-                                    <p>Integer molestie aliquam gravida. Nullam nec arcu finibus, imperdiet nulla vitae, placerat nibh. Cras maximus venenatis molestie.</p>
-                                    <div className="user-image">
-                                        <img src="http://placehold.it/60x60" alt="" />
-                                    </div>
-                                    <div className="team-info">
-                                        <h3 className="user-name">Kelvin Wood</h3>
-                                        <span>Digital Marketer</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* <!-- ***** Testimonials Item End ***** --> */}
-                        
-                        {/* <!-- ***** Testimonials Item Start ***** --> */}
-                        <div className="col-lg-4 col-md-6 col-sm-12">
-                            <div className="team-item">
-                                <div className="team-content">
-                                    <i><img src="assets/images/testimonial-icon.png" alt="" /></i>
-                                    <p>Quisque diam odio, maximus ac consectetur eu, auctor non lorem. Cras quis est non ante ultrices molestie. Ut vehicula et diam at aliquam.</p>
-                                    <div className="user-image">
-                                        <img src="http://placehold.it/60x60" alt="" />
-                                    </div>
-                                    <div className="team-info">
-                                        <h3 className="user-name">David Martin</h3>
-                                        <span>Website Manager</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        {/* <!-- ***** Testimonials Item End ***** --> */}
-                    </div>
-                </div>
-            </section>
-            {/* <!-- ***** Testimonials End ***** --> */}
+            <Testimonials />
 
             {/* <!-- ***** Pricing Plans Start ***** --> */}
             <section className="section colored" id="pricing-plans">
