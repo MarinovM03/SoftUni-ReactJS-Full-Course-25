feff2f12b7ede5663fe83da68eff86bae9c02859
Martin Marinov <martin.marinov10@yahoo.com>
2025-11-14 18:03:41 +0000

    Add Features component

--- 02-Introduction React, JSX and Components Exercise/html-template-demo/src/App.jsx
+++ 02-Introduction React, JSX and Components Exercise/html-template-demo/src/App.jsx
@@ -1,3 +1,5 @@
+import BigFeaturesLeft from "./components/BigFeaturesLeft.jsx"
+import Features from "./components/Features.jsx"
 import Footer from "./components/Footer.jsx"
 import Header from "./components/Header.jsx"
 import Preloader from "./components/Preloader.jsx"
@@ -12,80 +14,9 @@ function App() {
 
             <WelcomeArea />
 
+            <Features />
 
-            {/* ***** Features Small Start ***** */}
-            <section className="section home-feature">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="row">
-                                {/* ***** Features Small Item Start ***** */}
-                                <div className="col-lg-4 col-md-6 col-sm-6 col-12" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.2s">
-                                    <div className="features-small-item">
-                                        <div className="icon">
-                                            <i><img src="assets/images/featured-item-01.png" alt="" /></i>
-                                        </div>
-                                        <h5 className="features-title">Modern Strategy</h5>
-                                        <p>Customize anything in this template to fit your website needs</p>
-                                    </div>
-                                </div>
-                                {/* ***** Features Small Item End ***** */}
-
-                                {/* ***** Features Small Item Start ***** */}
-                                <div className="col-lg-4 col-md-6 col-sm-6 col-12" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.4s">
-                                    <div className="features-small-item">
-                                        <div className="icon">
-                                            <i><img src="assets/images/featured-item-01.png" alt="" /></i>
-                                        </div>
-                                        <h5 className="features-title">Best Relationship</h5>
-                                        <p>Contact us immediately if you have a question in mind</p>
-                                    </div>
-                                </div>
-                                {/* ***** Features Small Item End ***** */}
-
-                                {/* ***** Features Small Item Start ***** */}
-                                <div className="col-lg-4 col-md-6 col-sm-6 col-12" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.6s">
-                                    <div className="features-small-item">
-                                        <div className="icon">
-                                            <i><img src="assets/images/featured-item-01.png" alt="" /></i>
-                                        </div>
-                                        <h5 className="features-title">Ultimate Marketing</h5>
-                                        <p>You just need to tell your friends about our free templates</p>
-                                    </div>
-                                </div>
-                                {/* ***** Features Small Item End ***** */}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            {/* ***** Features Small End ***** */}
-
-            {/* ***** Features Big Item Start ***** */}
-            <section className="section padding-top-70 padding-bottom-0" id="features">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-5 col-md-12 col-sm-12 align-self-center" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
-                            <img src="assets/images/left-image.png" className="rounded img-fluid d-block mx-auto" alt="App" />
-                        </div>
-                        <div className="col-lg-1"></div>
-                        <div className="col-lg-6 col-md-12 col-sm-12 align-self-center mobile-top-fix">
-                            <div className="left-heading">
-                                <h2 className="section-title">Let’s discuss about you project</h2>
-                            </div>
-                            <div className="left-text">
-                                <p>Nullam sit amet purus libero. Etiam ullamcorper nisl ut augue blandit, at finibus leo efficitur. Nam gravida purus non sapien auctor, ut aliquam magna ullamcorper.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="hr"></div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            {/* ***** Features Big Item End ***** */}
+            <BigFeaturesLeft />
 
             {/* ***** Features Big Item Start ***** */}
             <section className="section padding-bottom-100">
