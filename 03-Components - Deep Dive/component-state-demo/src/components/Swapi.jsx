bcaeddec6759aceea657d05c42c65233ef0a1fbc
Martin Marinov <martin.marinov10@yahoo.com>
2025-11-22 22:14:28 +0000

    Add Swapi component to use as example for Fetch API

---
+++ 03-Components - Deep Dive/component-state-demo/src/components/Swapi.jsx
@@ -0,0 +1,11 @@
+export default function Swapi() {
+    return (
+        <section>
+            <h2>Star Wars Characters</h2>
+
+            <ul>
+                <li></li>
+            </ul>
+        </section>
+    );
+}
