70801e7f9b9d0f4346871d08f2f5678e683bdb43
Martin Marinov <martin.marinov10@yahoo.com>
2025-11-22 22:14:41 +0000

    Add SWAPI fetch to Swapi component

--- 03-Components - Deep Dive/component-state-demo/src/components/Swapi.jsx
+++ 03-Components - Deep Dive/component-state-demo/src/components/Swapi.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+
 export default function Swapi() {
+    useEffect(() => {
+        fetch('https://swapi.dev/api/people')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+            .catch(err => {
+                console.error(err.message);
+            })
+    }, []);
+
     return (
         <section>
             <h2>Star Wars Characters</h2>
