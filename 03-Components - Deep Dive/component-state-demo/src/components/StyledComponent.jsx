3588d0bebd27e5fe842b7b9ead5ffcb99c1bf947
Martin Marinov <martin.marinov10@yahoo.com>
2025-11-22 22:03:21 +0000

    Add CSS Module example in StyledComponent

--- 03-Components - Deep Dive/component-state-demo/src/components/StyledComponent.jsx
+++ 03-Components - Deep Dive/component-state-demo/src/components/StyledComponent.jsx
@@ -1,11 +1,11 @@
-import './StyledComponent.css'
+import styles from './StyledComponent.module.css';
 
 export default function StyledComponent() {
     return (
         <section>
             <h2>Styled Component</h2>
 
-            <p className="fancy-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, quidem reprehenderit beatae impedit ad consectetur distinctio laudantium voluptas quo nisi!</p>
+            <p className={styles['fancy-text']}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, quidem reprehenderit beatae impedit ad consectetur distinctio laudantium voluptas quo nisi!</p>
         </section>
     );
 }
