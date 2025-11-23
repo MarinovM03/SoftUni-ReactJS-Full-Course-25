836458b6d20c74fad020244179b812680ba464c8
Martin Marinov <martin.marinov10@yahoo.com>
2025-11-23 19:45:48 +0000

    Show dynamic user details data

--- client/src/components/UserDetails.jsx
+++ client/src/components/UserDetails.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { fromIsoDate } from "../utils/datetimeUtils.js";
+
 export default function UserDetails({
+    userId,
     onClose,
 }) {
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+        fetch(`http://localhost:3030/jsonstore/users/${userId}`)
+            .then(response => response.json())
+            .then(result => {
+                setUser(result);
+            })
+            .catch(err => alert(err.message));
+    }, [userId]);
+
     return (
         <div className="overlay">
             <div className="backdrop" onClick={onClose}></div>
@@ -32,41 +48,41 @@ export default function UserDetails({
                     <div className="content">
                         <div className="image-container">
                             <img
-                                src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
-                                alt=""
+                                src={user.imageUrl}
+                                alt={`${user.firstName}'s profile`}
                                 className="image"
                             />
                         </div>
 
                         <div className="user-details">
                             <p>
-                                User Id: <strong>62bb0c0eda039e2fdccba57b</strong>
+                                User Id: <strong>{user._id}</strong>
                             </p>
 
                             <p>
                                 Full Name:
-                                <strong> Peter Johnson </strong>
+                                <strong> {user.firstName} {user.lastName} </strong>
                             </p>
 
                             <p>
-                                Email: <strong>peter@abv.bg</strong>
+                                Email: <strong>{user.email}</strong>
                             </p>
 
                             <p>
-                                Phone Number: <strong>0812345678</strong>
+                                Phone Number: <strong>{user.phoneNumber}</strong>
                             </p>
 
                             <p>
                                 Address:
-                                <strong> Bulgaria, Sofia, Aleksandar Malinov 78 </strong>
+                                <strong> {user.address?.country}, {user.address?.city}, {user.address?.street} {user.address?.streetNumber} </strong>
                             </p>
 
                             <p>
-                                Created on: <strong>Wednesday, June 28, 2022</strong>
+                                Created on: <strong>{fromIsoDate(user.createdAt)}</strong>
                             </p>
 
                             <p>
-                                Modified on: <strong>Thursday, June 29, 2022</strong>
+                                Modified on: <strong>{fromIsoDate(user.updatedAt)}</strong>
                             </p>
                         </div>
                     </div>
