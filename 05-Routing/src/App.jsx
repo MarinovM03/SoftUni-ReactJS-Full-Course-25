b96b76ff2d87614197c0b92b69e96c8106738749
Martin Marinov <martin.marinov10@yahoo.com>
2025-11-26 05:17:18 +0000

    Add Home and About routes

--- 05-Routing/src/App.jsx
+++ 05-Routing/src/App.jsx
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router';
 import './App.css'
+import About from './components/About.jsx';
+import Home from './components/Home.jsx';
 
 function App() {
     return (
         <>
-            <h1>React Router</h1>
+        <h1>React Router</h1>
+        <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+        </Routes>
         </>
     );
 }
