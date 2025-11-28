4c3f3e90926aabd7d5a4e90c7e3ef59aca05aa38
Martin Marinov <martin.marinov10@yahoo.com>
2025-11-28 10:34:16 +0000

    Add Link and NavLink examples for redirecting

--- 05-Routing/src/App.jsx
+++ 05-Routing/src/App.jsx
@@ -1,15 +1,35 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route, Link, NavLink } from 'react-router';
 import './App.css'
 import Home from './components/Home.jsx';
 import About from './components/About.jsx';
 import NotFound from './components/NotFound.jsx';
 import City from './components/City.jsx';
 import Redirect from './components/Redirect.jsx';
+import styles from './App.module.css';
 
 function App() {
     return (
         <>
             <h1>React Router</h1>
+
+            {/* <nav>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/cities:plovdiv">City</Link>
+            </nav> */}
+
+            {/* <nav>
+                <NavLink style={({isActive}) => isActive ? {textDecoration: 'underline'} : {}} to="/">Home</NavLink>
+                <NavLink style={({isActive}) => isActive ? {textDecoration: 'underline'} : {}} to="/about">About</NavLink>
+                <NavLink style={({isActive}) => isActive ? {textDecoration: 'underline'} : {}} to="/cities:plovdiv">City</NavLink>
+            </nav> */}
+
+            <nav>
+                <NavLink className={({isActive}) => isActive ? styles['selected-link'] : ''} to="/">Home</NavLink>
+                <NavLink className={({isActive}) => isActive ? styles['selected-link'] : ''} to="/about">About</NavLink>
+                <NavLink className={({isActive}) => isActive ? styles['selected-link'] : ''} to="/cities:plovdiv">City</NavLink>
+            </nav>
+
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
