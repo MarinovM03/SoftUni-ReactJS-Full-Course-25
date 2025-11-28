a9d1dff3b6d64bb3848eb2f91b2d90118ed19cc8
Martin Marinov <martin.marinov10@yahoo.com>
2025-11-28 11:11:25 +0000

    Add route prefixes example

--- 05-Routing/src/App.jsx
+++ 05-Routing/src/App.jsx
@@ -5,6 +5,9 @@ import About from './components/About.jsx';
 import NotFound from './components/NotFound.jsx';
 import City from './components/City.jsx';
 import Redirect from './components/Redirect.jsx';
+import Dashboard from './components/Dashboard.jsx';
+import AdminUsers from './components/AdminUsers.jsx';
+import AdminPosts from './components/AdminPosts.jsx';
 import styles from './App.module.css';
 
 function App() {
@@ -12,11 +15,11 @@ function App() {
         <>
             <h1>React Router</h1>
 
-            {/* <nav>
+            <nav>
                 <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
                 <Link to="/cities:plovdiv">City</Link>
-            </nav> */}
+            </nav>
 
             {/* <nav>
                 <NavLink style={({isActive}) => isActive ? {textDecoration: 'underline'} : {}} to="/">Home</NavLink>
@@ -24,17 +27,24 @@ function App() {
                 <NavLink style={({isActive}) => isActive ? {textDecoration: 'underline'} : {}} to="/cities:plovdiv">City</NavLink>
             </nav> */}
 
-            <nav>
-                <NavLink className={({isActive}) => isActive ? styles['selected-link'] : ''} to="/">Home</NavLink>
-                <NavLink className={({isActive}) => isActive ? styles['selected-link'] : ''} to="/about">About</NavLink>
-                <NavLink className={({isActive}) => isActive ? styles['selected-link'] : ''} to="/cities:plovdiv">City</NavLink>
-            </nav>
+            {/* <nav>
+                <NavLink className={(isActive) => isActive ? styles['selected-link'] : ''} to="/">Home</NavLink>
+                <NavLink className={(isActive) => styles['selected-link']} to="/about">About</NavLink>
+                <NavLink className={(isActive) => styles['selected-link']} to="/cities:plovdiv">City</NavLink>
+            </nav> */}
 
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
                 <Route path='/cities/:city' element={<City />} />
                 <Route path='/redirect' element={<Redirect />} />
+
+                <Route path='/admin'>
+                    <Route path='dashboard' element={<Dashboard />} />
+                    <Route path='users' element={<AdminUsers />} />
+                    <Route path='posts' element={<AdminPosts />} />
+                </Route>
+
                 <Route path='*' element={<NotFound />} />
             </Routes>
         </>
